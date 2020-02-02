8bf3882d187a739342790f67950006d0dd23380a
Alex Pavlenko <oleksa.pavlenko@gmail.com>
2020-02-02 09:32:02 +0200

    Implement Assignment1Part1.

---
+++ src/com/shpp/diraven/p2p/Assignment1Part1.java
@@ -0,0 +1,29 @@
+package com.shpp.diraven.p2p;
+
+public class Assignment1Part1 extends MyKarel {
+
+    public void run() throws Exception {
+        go_to_newspaper();
+        pick_newspaper();
+        go_to_start();
+    }
+
+    private void go_to_newspaper() throws Exception {
+        turnRight();
+        move();
+        turnLeft();
+        move_until_beeper();
+    }
+
+    private void pick_newspaper() throws Exception {
+        pickBeeper();
+    }
+
+    private void go_to_start() throws Exception {
+        turnBack();
+        move_until_obstacle();
+        turnRight();
+        move_until_obstacle();
+        turnRight();
+    }
+}
--- src/com/shpp/diraven/p2p/MyKarel.java
+++ src/com/shpp/diraven/p2p/MyKarel.java
@@ -3,9 +3,29 @@ package com.shpp.diraven.p2p;
 import com.shpp.karel.KarelTheRobot;
 
 public class MyKarel extends KarelTheRobot {
-    public void run() throws Exception {
-        turnLeft();
-        turnLeft();
-        move();
+    protected void turnLeftTimes(int times) throws Exception {
+        for (int i = 0; i < times; i++) {
+            turnLeft();
+        }
+    }
+
+    protected void turnRight() throws Exception {
+        turnLeftTimes(3);
+    }
+
+    protected void turnBack() throws Exception {
+        turnLeftTimes(2);
+    }
+
+    protected void move_until_obstacle() throws Exception {
+        while (frontIsClear()) {
+            move();
+        }
+    }
+
+    protected void move_until_beeper() throws Exception {
+        do {
+            move();
+        } while (!beepersPresent());
     }
 }
