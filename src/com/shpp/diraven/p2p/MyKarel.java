3e95886deb9006501aa582c32035648487b6f4c7
Alex Pavlenko <oleksa.pavlenko@gmail.com>
2020-02-02 08:52:51 +0200

    Create base class.

---
+++ src/com/shpp/diraven/p2p/MyKarel.java
@@ -0,0 +1,11 @@
+package com.shpp.diraven.p2p;
+
+import com.shpp.karel.KarelTheRobot;
+
+public class MyKarel extends KarelTheRobot {
+    public void run() throws Exception {
+        turnLeft();
+        turnLeft();
+        move();
+    }
+}
