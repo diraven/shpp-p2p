4226507cde3ac8a70496cfca21a05674abde0117
Alex Pavlenko <oleksa.pavlenko@gmail.com>
2020-02-04 09:58:02 +0200

    Better comments for a1p2.

--- src/com/shpp/p2p/cs/opavlenko/assignment1/Assignment1Part2.java
+++ src/com/shpp/p2p/cs/opavlenko/assignment1/Assignment1Part2.java
@@ -3,20 +3,26 @@ package com.shpp.p2p.cs.opavlenko.assignment1;
 public class Assignment1Part2 extends MyKarel {
 
     public void run() throws Exception {
-        fillLine();
-
+        fill();
         while (frontIsClear()) {
-            for (int i = 0; i < 4; i++) {
-                move();
-            }
-            fillLine();
+            next();
+            fill();
+        }
+    }
+
+    /*
+    Moves bot to the next column.
+     */
+    private void next() throws Exception {
+        for (int i = 0; i < 4; i++) {
+            move();
         }
     }
 
     /*
     Fills the line with beepers and returns Karel to the starting position on the line.
      */
-    private void fillLine() throws Exception {
+    private void fill() throws Exception {
         turnLeft();
         while (frontIsClear()) {
             if (!beepersPresent()) {
